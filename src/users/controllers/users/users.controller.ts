37d9062dd681344706db371f26bbefa07e304088
moukmessie <moukmessie@gmail.com>
2023-03-14 23:00:00 +0100

    Services & Providers

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -2,6 +2,8 @@ import {
   Body,
   Controller,
   Get,
+  HttpException,
+  HttpStatus,
   Param,
   ParseIntPipe,
   Post,
@@ -13,28 +15,30 @@ import {
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { CreateUserDto } from '../../dtos/CreateUser.dto';
+import { UsersService } from '../../services/users/users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private userService: UsersService) {}
   @Get()
-  getUsers(@Query('sortBy') sortBy: string) {
-    console.log(sortBy);
-    return { username: 'Messie', email: 'mouk@gmail.com' };
+  getUsers() {
+    return this.userService.fetchUsers();
   }
 
   @Post('create')
   @UsePipes(new ValidationPipe())
   createUsers(@Body() userData: CreateUserDto) {
-    console.log(userData);
-    return {};
+    return this.userService.createUser(userData);
   }
 
   @Get(':id')
   getUserById(@Param('id', ParseIntPipe) id: number) {
     console.log(id);
-    return {
-      id,
-    };
+    const user = this.userService.fetchUserById(id);
+
+    if (!user)
+      throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+    return user;
   }
 
   //   @Post('create')
--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserType } from '../../../../dist/utils/types';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private fakeUsers = [
+    { username: 'Messiemg', email: 'mouk@gmail.com' },
+    { username: 'Mouk', email: 'mouk@gmail.com' },
+    { username: 'mg', email: 'mg@gmail.com' },
+  ];
+  fetchUsers() {
+    return this.fakeUsers;
+  }
+
+  createUser(userData: CreateUserType) {
+    console.log(userData);
+    this.fakeUsers.push(userData);
+    return;
+  }
+
+  fetchUserById(id: number) {
+    return null;
+  }
+}
